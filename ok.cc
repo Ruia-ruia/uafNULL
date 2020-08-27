f7155ddc8fc66a8b97e8f5e6f5e42043afb23aff
Ruia-ruia <61450241+Ruia-ruia@users.noreply.github.com>
2020-08-27 15:47:10 +0100

    Update ok.cc

--- ok.cc
+++ ok.cc
@@ -18,12 +18,13 @@ public:
 	}
 
 	void Access_After_Free() {
-		if (!client_ptr->is_alive()) {
-			cout << "accessed" << endl;
+		if (client_ptr) {
+			if (!client_ptr->is_alive()) {
+				cout << "accessed" << endl;
+			}
 		}
 	}
 
-private:
 	Client_Class* client_ptr;
 };
 
@@ -40,6 +41,7 @@ int main() {
 	//deletes object pointed to by render_frame_host
 	Delete_Obj(render_frame_host);
 
+	TC->client_ptr = nullptr;
 	//TC outlives render_frame_host and then accesses it
 	//without observing its lifetime
 	TC->Access_After_Free();
